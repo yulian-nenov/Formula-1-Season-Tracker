05b790808650da56b0c191e6b90066126e4f9abc
July132 <july.nenov.sf@gmail.com>
2026-02-10 23:23:03 +0200

    Created common app, made home page template, added static files

--- FormulaSeasonTracker/asgi.py
+++ FormulaSeasonTracker/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FormulaDriverTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FormulaSeasonTracker.settings')
 
 application = get_asgi_application()
