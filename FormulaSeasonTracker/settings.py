d1064d0a5ac11df08476f5a01aab5fb9af71c8a1
July132 <july.nenov.sf@gmail.com>
2026-04-05 02:36:03 +0300

    Added storage for media files

--- FormulaSeasonTracker/settings.py
+++ FormulaSeasonTracker/settings.py
@@ -147,6 +147,9 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
